d8ec88d89d7ed0e2f58dfd4599db1b11fda05b7c
Muhammed Nafees <ahsannafees909@gmail.com>
2023-05-04 09:33:01 -0700

    openmp

--- dgemv-openmp.cpp
+++ dgemv-openmp.cpp
@@ -3,7 +3,7 @@
 #include <stdio.h>
 #include <omp.h>
 
-const char* dgemv_desc = "OpenMP dgemv.";
+const char *dgemv_desc = "OpenMP dgemv.";
 
 /*
  * This routine performs a dgemv operation
@@ -12,18 +12,30 @@ const char* dgemv_desc = "OpenMP dgemv.";
  * On exit, A and X maintain their input values.
  */
 
-void my_dgemv(int n, double* A, double* x, double* y) {
+void my_dgemv(int n, double *A, double *x, double *y)
+{
 
-   #pragma omp parallel
-   {
-      int nthreads = omp_get_num_threads();
-      int thread_id = omp_get_thread_num();
-      printf("Hello world: thread %d of %d checking in. \n", thread_id, nthreads);
-   }
+   // #pragma omp parallel
+   //    {
+   //       int nthreads = omp_get_num_threads();
+   //       int thread_id = omp_get_thread_num();
+   //       printf("Hello world: thread %d of %d checking in. \n", thread_id, nthreads);
+   //    }
 
    // insert your dgemv code here. you may need to create additional parallel regions,
    // and you may want to comment out the above parallel code block that prints out
    // nthreads and thread_id so as to not taint your timings
 
-}
+#pragma omp for
+   for (int i = 0; i < n; i++)
+   {
+#pragma omp atomic
+      y[i] += A[i * n] * x[0];
 
+      for (int j = 1; j < n; j++)
+      {
+#pragma omp atomic
+         y[i] += A[i * n + j] * x[j];
+      }
+   }
+}
